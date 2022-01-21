52ecd7c6e14a11e865c7eabce52b363c7e110bde
codingbaby <vsmysee@gmail.com>
2022-01-21 22:42:37 +0800

    add

---
+++ cmake-build-debug/CMakeFiles/althttpd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/althttpd.dir/main.c.o"
+  "althttpd"
+  "althttpd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/althttpd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
